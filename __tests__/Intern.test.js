33b9e4dd24f0bc81731063afc180f3e7a0cf2243
Reid Schroder <reidschroder2@gmail.com>
2022-02-22 16:52:57 -0500

    edited intern test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,7 +1,7 @@
 const Intern = require("../lib/Intern.js");
 
 test('creates intern object', () => {
-    const intern = new Intern("reid", 3, "reidschroder2@gmail.com", "Intern", "school");
+    const intern = new Intern("reid", 4, "reidschroder2@gmail.com", "Intern", "school");
 
     expect(intern.school).toEqual(expect.any(String));
 });
